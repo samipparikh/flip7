67aa710e7f749295642643138f50a109a6f14920
Samip Parikh <samippar@amazon.com>
2026-05-21 10:38:51 -0700

    Add Firebase config for online multiplayer

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,14 @@
  * 6. Replace the placeholder values below with your config
  */
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAa7-2TnMXUlaiQwuO3nsNlgj38Ejdp6WU",
+  authDomain: "flip7-35782.firebaseapp.com",
+  databaseURL: "https://flip7-35782-default-rtdb.firebaseio.com",
+  projectId: "flip7-35782",
+  storageBucket: "flip7-35782.firebasestorage.app",
+  messagingSenderId: "424240528171",
+  appId: "1:424240528171:web:08e11beb043c8ca0c0236e",
+  measurementId: "G-YNVD6GK1P9"
 };
